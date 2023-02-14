5aa35434ab48216dc8e44e65f9c41d71110c3012
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-14 13:26:29 +0000

    Task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,4 +4,4 @@
 
 def list_all(mongo_collection):
     """List all documents in a collection"""
-    return [doc for doc in mongo_collection.find()]
+    return [doc for doc in mongo_collection.find({})]
