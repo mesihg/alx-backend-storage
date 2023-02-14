bcf145504bd5b8697b8ae0dc23223058af57c92b
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-14 13:34:05 +0000

    Task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 10 module"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a collection's document based on name"""
+    mongo_collection.update_many(
+        {'name': name},
+        {$set: {'topics': topics}}
+    )
+    return result.inserted_id
