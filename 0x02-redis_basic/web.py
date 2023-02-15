99443bb837132887099b2c01ca3779439e9ce6f1
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-15 13:42:48 +0000

    Task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-""" Task 5 Module """
-import requests
+"""Task 5 module"""
 import redis
+import requests
 from functools import wraps
+from typing import Callable
 
-store = redis.Redis()
+
+redis_store = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
+def count_requests(method: Callable) -> Callable:
+    """Caches output of fetched data"""
     @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
         html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
+        redis_.setex(f"cached:{url}", 10, html)
         return html
+
     return wrapper
 
 
-@count_url_access
+@count_requests
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """Return the content of a URL after caching the response"""
+    req = requests.get(url)
+    return req.text
