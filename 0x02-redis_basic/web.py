6c83f76481b3a539849579d23838c72a98898385
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-15 13:41:48 +0000

    Task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 """ Task 5 Module """
-from functools import wraps
-import redis
 import requests
+import redis
+from functools import wraps
 
-redis_ = redis.Redis()
+store = redis.Redis()
 
 
-def count_requests(method):
-    """ Caches output of fetched data """
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
     def wrapper(url):
-        """ Wrapper for decorator """
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@count_requests
+@count_url_access
 def get_page(url: str) -> str:
-    """ Return the content of a URL after caching the response """
-    req = requests.get(url)
-    return req.text
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
